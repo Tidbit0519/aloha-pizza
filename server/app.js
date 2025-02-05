0c1ae023a92f25b1d995332154bda31e94153718
Jason Ban Tze Tan <jtbt1218@gmail.com>
2025-02-05 13:35:37 -0700

    Rename entry point index.js to app.js

--- server/app.js
+++ server/app.js
@@ -1,7 +1,8 @@
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
-import connectDB from "./config/db.js";
+import swaggerUi from "swagger-ui-express";
+import { connectDB, swaggerSpec } from "./config/index.js";
 
 const app = express();
 app.use(cors());
@@ -11,8 +12,9 @@ dotenv.config();
 connectDB();
 
 app.get("/", (req, res) => {
-    res.send("API is running...");
+    res.send("API is running");
 });
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
