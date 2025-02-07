6c46869c9eef91d06fa0817b2b9c6a76413435d6
Jason Ban Tze Tan <jtbt1218@gmail.com>
2025-02-07 15:25:19 -0700

    Create PizzaList.test.jsx

---
+++ client/src/tests/components/PizzaList.test.jsx
@@ -0,0 +1,48 @@
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import PizzaList from "../../components/PizzaList";
+
+const pizzas = [
+	{
+		_id: "1",
+		name: "Pizza 1",
+		toppings: [
+			{
+				_id: "1",
+				name: "topping 1",
+			},
+		],
+	},
+	{
+		_id: "2",
+		name: "Pizza 2",
+		toppings: [{ _id: "2", name: "topping 2" }],
+	},
+];
+
+describe("PizzaList", () => {
+	it("renders the PizzaList with no pizzas", () => {
+		render(
+			<PizzaList
+				pizzas={[]}
+				updatePizza={() => {}}
+				deletePizza={() => {}}
+			/>
+		);
+		expect(screen.getByText("No pizzas available")).toBeInTheDocument();
+	});
+
+	it("renders the PizzaList with pizzas", () => {
+		render(
+			<PizzaList
+				pizzas={pizzas}
+				updatePizza={() => {}}
+				deletePizza={() => {}}
+			/>
+		);
+		expect(screen.getByText("Pizza 1")).toBeInTheDocument();
+		expect(screen.getByText("Pizza 2")).toBeInTheDocument();
+		expect(screen.getByText("topping 1")).toBeInTheDocument();
+		expect(screen.getByText("topping 2")).toBeInTheDocument();
+	});
+});
