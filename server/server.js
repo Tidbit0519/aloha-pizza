875051ff280aeb09aa0d471ec6b610057bf77367
Jason Ban Tze Tan <jtbt1218@gmail.com>
2025-02-08 00:22:32 -0700

    Configure server to read from root .env

--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
 import app from "./app.js";
 import { dbConnect } from "./config/index.js";
-import dotenv from "dotenv";
+import { fileURLToPath } from 'url';
+import path from 'path';
+import dotenv from 'dotenv';
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({
+    path: path.resolve(__dirname, '../.env')
+});
 dbConnect();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
